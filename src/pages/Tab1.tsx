4a4517f912f5b54072f3e1b994003f946d099ccb
Tiao <1314tiao@gmail.com>
2024-11-30 21:02:11 +0000

    Inital Commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,24 +1,62 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import { IonContent, IonHeader, IonToolbar, IonTitle, IonButton, IonCard, IonList, IonItem, IonLabel, IonSearchbar } from '@ionic/react';
+import './Tab1.css'; // Import external CSS
 
-const Tab1: React.FC = () => {
+const Tab1 = () => { 
   return (
-    <IonPage>
-      <IonHeader>
+    <div className="tab1-container">
+      <IonHeader className="ion-header">
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Search</IonTitle>
+          <div className="header-notifications"></div> {/* Notification Icon */}
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+      <IonContent>
+        {/* Search Bar */}
+        <IonSearchbar 
+          debounce={0} 
+          placeholder="Search for Medications" 
+          className="search-bar" 
+        />
+
+        {/* User and Medication Details */}
+        <IonCard className="user-medication-card">
+          <IonItem>
+            <IonLabel className="ion-label">User: Tiao</IonLabel> 
+          </IonItem>
+          <IonItem>
+            <IonLabel className="ion-label">Medication: Le</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel className="ion-label">Count:</IonLabel>
+            <IonLabel>Lenvima, Lenvatinib, Levalidomide, Letrozale</IonLabel>
+          </IonItem>
+        </IonCard>
+
+        {/* Medication List */}
+        <IonCard className="medication-list-card">
+          <IonItem>
+            <IonLabel className="ion-label">List of meds / amounts</IonLabel>
+          </IonItem>
+          <IonList>
+            <IonItem>
+              <IonLabel>Lexapro 2x</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Protamine 4x</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Zavciefa 10x</IonLabel>
+            </IonItem>
+          </IonList>
+        </IonCard>
+
+        {/* Submit Button */}
+        <IonButton expand="full" className="submit-button">
+          Submit
+        </IonButton>
       </IonContent>
-    </IonPage>
+    </div>
   );
 };
 
